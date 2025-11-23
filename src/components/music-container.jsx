469af0335ab129f042a26cfbeceb5e9845da2a88
Phil <182430206+Fico27@users.noreply.github.com>
2025-11-23 14:55:34 +0100

    Fixed an issue with the hero that was preventing overflow and taking up the entire screen. fixed container issue with music-container. Added musicContainer to homepage for testing.

--- src/components/music-container.jsx
+++ src/components/music-container.jsx
@@ -4,28 +4,29 @@ function MusicContainer() {
   return (
     <div className="music-container">
       <h2>Check out my latest</h2>
-      <div className="video-container"></div>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/P2rNC3xkvrE?si=nq_x0tRfVB76wjiG"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/P2rNC3xkvrE?si=nq_x0tRfVB76wjiG"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/gdu9cS7guqQ?si=ZUKK4YBre6BGQ-BY"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/gdu9cS7guqQ?si=ZUKK4YBre6BGQ-BY"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,15 @@
 import Nav from "../components/nav";
 import Hero from "../components/hero";
 import Footer from "../components/footer";
+import MusicContainer from "../components/music-container";
 
 function Homepage() {
   return (
     <div className="page">
       <Nav></Nav>
       <Hero></Hero>
+
+      <MusicContainer></MusicContainer>
       <Footer></Footer>
     </div>
   );
