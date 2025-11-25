3a2213874778d7dce4c17cca152e645cf9b56a37
Phil <182430206+Fico27@users.noreply.github.com>
2025-11-25 10:52:34 +0100

    added youtube link to the footer with some styling.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,13 @@ function Footer() {
   return (
     <footer>
       <p>&copy; 2025 St. Srd Perisa. All rights reserved.</p>
+      <a href="https://www.youtube.com/@Natikache/videos">
+        <img
+          src="/youtube-icon.svg"
+          alt="YouTube"
+          className="social-icon invert"
+        />
+      </a>
     </footer>
   );
 }
